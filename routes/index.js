3091561b9574bd7965bbc06d957443d795bd5683
David Hull <david.hull37@gmail.com>
2018-11-30 19:33:51 -0600

    Started designing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const app = express();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.get('/', function(req, res, next) {
+  return res.render('indexPage', { title: 'Express' });
 });
 
-module.exports = router;
+module.exports = app;
