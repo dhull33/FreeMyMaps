a7e4783252e08203a7495168d7739831b5059b93
David Hull <david.hull37@gmail.com>
2018-12-14 11:25:01 -0600

    Renamed signup function to sendUserBack

--- public/javascripts/auth/signUp.js
+++ public/javascripts/auth/signUp.js
@@ -1,8 +1,8 @@
 // Get sign up form data and submit to backend
-const sendNewUser = () => {
+const sendNewUserBack = () => {
   return $('#signupForm').on('submit', (event) => {
     event.preventDefault();
     console.log(event);
   });
 };
-sendNewUser();
+sendNewUserBack();
