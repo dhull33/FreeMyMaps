c4c2ec453f6a29c9fc5dc78ca5e3f284df9b21f8
David Hull <david.hull37@gmail.com>
2018-12-14 18:31:04 -0600

    Began working on authentication with passportjs

---
+++ authentication/init.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+const db = require('../data/dataBase');
+
+module.exportsArgument = () => {
+  passport.serializeUser((user, done) => {
+    return done(null, user);
+  });
+
+  passport.deserializeUser((user, done) => {
+    db.one('SELECT * FROM user WHERE user_id=$1', [user.user_id])
+      .then((myUser) => {
+        return done(null, myUser);
+      })
+      .catch((error) => {
+        return done(error, null);
+      });
+  });
+};
