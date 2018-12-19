01f01d186fa7411011b951b5fb9cd41340d6ee37
David Hull <david.hull37@gmail.com>
2018-12-18 19:31:42 -0600

    Added free signup route

--- routes/authentication/signup.js
+++ routes/authentication/signup.js
@@ -3,3 +3,7 @@ const passport = require('../../authentication/passportLocal');
 const authHelpers = require('../../authentication/authHelpers');
 
 const app = express();
+
+app.post('/auth/signup/free', (req, res) => {});
+
+module.exports = app;
