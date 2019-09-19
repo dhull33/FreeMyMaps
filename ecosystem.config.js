bd1ab8f3c9eb62a8ce44a27af6b290645f4f087f
David Hull <david.hull37@gmail.com>
2019-09-19 12:10:21 -0500

    Removed some comments

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,15 +15,4 @@ module.exports = {
       }
     }
   ]
-
-  // deploy: {
-  //   production: {
-  //     user: 'node',
-  //     host: '212.83.163.1',
-  //     ref: 'origin/master',
-  //     repo: 'git@github.com:repo.git',
-  //     path: '/var/www/production',
-  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
-  //   }
-  // }
 };
